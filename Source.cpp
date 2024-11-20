d47df64f771c9fa568a120f7aab11391f1ec274e
pamela <mamedovazamira04@gmail.com>
2024-11-20 23:05:36 +0800

    first commit

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	unsigned int testUnit = 65534;//oxfffe
+	cout << "output in unsigned int 1 type:"<<testUnit<<endl;//<<oct;
+	cout << "output in char type:!" << static_cast<char>(testUnit) << endl;
+	cout << "output in short type:" << static_cast<short>(testUnit) << endl;
+	cout << "output in int type:" << static_cast<int>(testUnit) << endl;
+	cout << "output in double type:" << static_cast<double>(testUnit) << endl;
+	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnit) << endl;
+	cout << "output in Hex unsigned int type:" << hex << testUnit << endl;
+	return 0;
+}
